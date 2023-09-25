e618aea1848b4ffe4db13827d4cffe9fc15b4a34
nikitamessi69 <nzhuravlev92@gmail.com>
2023-09-25 10:37:29 +0300

    add hw2

--- Task_4.py
+++ Task_4.py
@@ -1,20 +1,18 @@
-# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем. 
+# Напишите программу, которая принимает две строки вида “a/b” - дробь с числителем и знаменателем.
 # Программа должна возвращать сумму и произведение* дробей. Для проверки своего кода используйте модуль fractions.
 
 from fractions import Fraction
 
 def calculate_fraction_operations(fraction1, fraction2):
-    # Преобразуем строки в дроби с помощью модуля fractions
     fraction1 = Fraction(fraction1)
     fraction2 = Fraction(fraction2)
 
-    # Вычисляем сумму и произведение дробей
     sum_result = fraction1 + fraction2
     product_result = fraction1 * fraction2
 
     return sum_result, product_result
 
-# Пример использования
+
 fraction1 = input("Введите первую дробь (в формате a/b): ")
 fraction2 = input("Введите вторую дробь (в формате a/b): ")
 
