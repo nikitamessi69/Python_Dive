cf972c2512268773487992f85167d2da266ddddc
nikitamessi69 <nzhuravlev92@gmail.com>
2023-10-01 19:27:57 +0300

    HW_3

--- Task_5.py
+++ Task_5.py
@@ -1,3 +1,14 @@
 # Дан список повторяющихся элементов. Вернуть список с дублирующимися элементами. 
 # В результирующем списке не должно быть дубликатов.
 # [1, 2, 3, 1, 2] -> [1, 2]
+
+def find_duplicates(lst):
+    duplicates = []
+    for item in lst:
+        if lst.count(item) > 1 and item not in duplicates:
+            duplicates.append(item)
+    return duplicates
+
+lst = [1, 2, 3, 1, 2]
+result = find_duplicates(lst)
+print(result)
--- Task_7.py
+++ Task_7.py
@@ -2,3 +2,27 @@
 # Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность. 
 # Достаточно вернуть один допустимый вариант.
 # *Верните все возможные варианты комплектации рюкзака.
+
+
+def fill_backpack(items, max_weight):
+    sorted_items = sorted(items.items(), key=lambda x: x[1])
+    backpack = {}
+    total_weight = 0
+
+    for item, weight in sorted_items:
+        if total_weight + weight <= max_weight:
+            backpack[item] = weight
+            total_weight += weight
+
+    return backpack
+
+items = {
+    'Тент': 2,
+    'Спальник': 1,
+    'Еда': 3,
+    'Вода': 2,
+    'Кружка': 0.5
+}
+max_weight = 5
+result = fill_backpack(items, max_weight)
+print(result)
